df98c0aa909f10e922ceb421c3c5239fb565cf77
Raul Larriega <raulgf_02@hotmail.com>
2018-08-30 11:36:56 -0500

    persistence of data +1

--- persistencia/scores.py
+++ persistencia/scores.py
@@ -1,4 +1,5 @@
 def save_scores(name_file, scores):
+    #w is only write
     file = open(name_file,"w")
     for name, score, time in scores:
         file.write(name+ ","+str(score)+","+time+"\n")
@@ -6,8 +7,10 @@ def save_scores(name_file, scores):
 
 def get_scores(name_file):
     scores = []
+    #r is only read
     file = open(name_file,"r")
     for line in file:
+        # rstrip remove character "\n"
         name, score, time = line.rstrip("\n").split(",")
         scores.append((name,int(score), time))
     file.close()
