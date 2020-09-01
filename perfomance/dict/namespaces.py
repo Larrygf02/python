a1f9eee4bc2859037525c38697442c07196fa4bb
Larrygf02 <raulgf_02@hotmail.com>
2020-09-01 00:00:58 -0500

    namespace dict

--- perfomance/dict/namespaces.py
+++ perfomance/dict/namespaces.py
@@ -1,1 +1,28 @@
 # page 103
+import time
+import math
+from math import sin
+
+def test1(x):
+    return math.sin(x)
+
+def test2(x):
+    return sin(x)
+
+def test3(x, localsin=math.sin):
+    return localsin(x)
+
+start = time.time()
+test = [test1(i) for i in range(10000000)]
+end = time.time()
+print(end-start)
+
+start = time.time()
+test2 = [test2(i) for i in range(10000000)]
+end = time.time()
+print(end-start)
+
+start = time.time()
+test3 = [test3(i) for i in range(10000000)]
+end = time.time()
+print(end-start)
